582c802c675482e6fe24f159d7226f4dbd038332
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-05-12 19:57:32 +0600

    check

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,35 +1,99 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import UserCard from "./UserCard";
 
 const User = ({ UserPromise }) => {
-  const users = use(UserPromise);
-  console.log(users);
+  const initialUsers = use(UserPromise);
+//   console.log(users);
+  const [users,setUser]=useState(initialUsers)
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const target = e.target;
+    const name = target.name.value;
+    const username = target.username.value;
+    const email = target.email.value;
+    const phone = target.phone.value;
+    const website = target.website.value;
+
+    const user = { name, username, email, phone, website };
+    console.log(user);
+
+
+    fetch("http://localhost:4000/users",{
+        method:'POST',
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body:JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        const newUser=[...users,data];
+        setUser(newUser);
+    })
+
+
+  };
   return (
     <div className="w-11/12 mx-auto">
       <div className=" flex flex-col justify-center m-11/12 mx-auto ">
-        <form action="">
+        <form onSubmit={handleAddUser}>
           <div className="">
             <legend className="fieldset-legend">Name</legend>
-            <input name="name" type="text" className="input" placeholder="Type here" />
+            <input
+              name="name"
+              type="text"
+              className="input"
+              placeholder="Type here"
+            />
           </div>
-           <div className="">
+          <div className="">
             <legend className="fieldset-legend">UserName</legend>
-            <input name="username" type="text" className="input" placeholder="Type here" />
+            <input
+              name="username"
+              type="text"
+              className="input"
+              placeholder="Type here"
+            />
           </div>
-
-           <div className="">
+          <div className="">
             <legend className="fieldset-legend">Email</legend>
-            <input name="email" type="email" className="input" placeholder="Type here" />
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Type here"
+            />
           </div>
-
-           <div className="">
+          <div className="">
             <legend className="fieldset-legend">Phone</legend>
-            <input  name="phone" type="text" className="input" placeholder="Type here" />
+            <input
+              name="phone"
+              type="text"
+              className="input"
+              placeholder="Type here"
+            />
           </div>
-
-           <div className="">
+          <div className="">
             <legend className="fieldset-legend">website</legend>
-            <input name="website" type="text" className="input" placeholder="Type here" />
+            <input
+              name="website"
+              type="text"
+              className="input"
+              placeholder="Type here"
+            />
+          </div>{" "}
+          <br />
+          <div className=" cursor-pointer">
+            {/* <legend className="fieldset-legend">website</legend> */}
+            <input
+              name="website"
+              type="submit"
+              value="addUser"
+              className="input cursor-pointer"
+              placeholder="Type here"
+            />
           </div>
         </form>
       </div>
